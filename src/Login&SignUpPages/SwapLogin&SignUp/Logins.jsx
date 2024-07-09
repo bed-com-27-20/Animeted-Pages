2361f1c749e225f6062675170c672884019ab395
Tafadzwa Gambiza <bed-com-27-20@unima.ac.mw>
2024-07-09 10:01:06 +0200

    made the div that need to be swaped

--- src/Login&SignUpPages/SwapLogin&SignUp/Logins.jsx
+++ src/Login&SignUpPages/SwapLogin&SignUp/Logins.jsx
@@ -3,8 +3,26 @@ import './Logins.css'
 
 export default function Logins() {
   return (
-    <div>
-      Login s
+    <div className='fswapdiv'>
+        <div className='sswapdiv'>
+            <div className='ssffdiv1'>
+                sign in
+            </div>
+            <div className='ssffdiv2'>
+                hello 
+                friends
+            </div>
+        </div>
+        <div className='sswapdiv'>
+            <div className='ssffdiv1'>
+                welcom 
+                back
+            </div>
+            <div className='ssffdiv2'>
+                create 
+                account
+            </div>
+        </div>
     </div>
   )
 }
