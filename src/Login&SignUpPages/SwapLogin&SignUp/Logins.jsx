6bedf677c7465d15e3e2cb57cbfee315e5b9c94c
Tafadzwa Gambiza <bed-com-27-20@unima.ac.mw>
2024-07-09 15:09:10 +0200

    made the div that need to be swaped into the login

--- src/Login&SignUpPages/SwapLogin&SignUp/Logins.jsx
+++ src/Login&SignUpPages/SwapLogin&SignUp/Logins.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Logins.css'
 
-export default function Logins() {
+const Logins = () => {
+  const [showDiv2, setShowDiv2] = useState(true);
+
+  const handleSwap = () => {
+    setShowDiv2(!showDiv2);
+  };
+
   return (
     <div className='fswapdiv'>
+      {showDiv2 ? (
         <div className='sswapdiv'>
-            <div className='ssffdiv1'>
+          <div className='ssffdiv1'>
                 sign in
             </div>
             <div className='ssffdiv2'>
                 hello 
                 friends
+                <button onClick={handleSwap} className='swapbtz'>go to acc create</button>
             </div>
         </div>
+      ) : (
         <div className='sswapdiv'>
-            <div className='ssffdiv1'>
+          <div className='ssffdiv1'>
                 welcom 
                 back
             </div>
             <div className='ssffdiv2'>
                 create 
                 account
+                <button onClick={handleSwap} className="swap-button">go to sign up</button> 
             </div>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default Logins;
