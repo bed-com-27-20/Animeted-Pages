b292de325f099e5b8cd6320cd5c6eb4009e9c442
Tafadzwa Gambiza <bed-com-27-20@unima.ac.mw>
2024-07-10 17:46:42 +0200

    done with the swapping of pages and div's now left to add content

--- src/Login&SignUpPages/SwapLogin&SignUp/Logins.jsx
+++ src/Login&SignUpPages/SwapLogin&SignUp/Logins.jsx
@@ -3,19 +3,23 @@ import './Logins.css'
 
 const Logins = () => {
   const [showDiv2, setShowDiv2] = useState(true);
+  const [isSwapped, setIsSwapped] = useState(false);
 
   const handleSwap = () => {
-    setShowDiv2(!showDiv2);
+    setIsSwapped(!isSwapped);
+    setTimeout(() => {
+      setShowDiv2(!showDiv2);
+    }, 1000); // 1000 milliseconds = 5 seconds
   };
 
   return (
     <div className='fswapdiv'>
       {showDiv2 ? (
         <div className='sswapdiv'>
-          <div className='ssffdiv1'>
+          <div className={`ssffdiv1 ${isSwapped ? 'swapped' : ''}`}>
                 sign in
             </div>
-            <div className='ssffdiv2'>
+            <div className={`ssffdiv2 ${isSwapped ? 'swapped' : ''}`}>
                 hello 
                 friends
                 <button onClick={handleSwap} className='swapbtz'>go to acc create</button>
@@ -23,11 +27,11 @@ const Logins = () => {
         </div>
       ) : (
         <div className='sswapdiv'>
-          <div className='ssffdiv1'>
+          <div className={`ssffdiv3 ${isSwapped ? 'swapped' : ''}`}>
                 welcom 
                 back
             </div>
-            <div className='ssffdiv2'>
+            <div className={`ssffdiv4 ${isSwapped ? 'swapped' : ''}`}>
                 create 
                 account
                 <button onClick={handleSwap} className="swap-button">go to sign up</button> 
