1dfab9b4ed7ef8a295bdc8a178b65c6fc766454e
Tafadzwa Gambiza <bed-com-27-20@unima.ac.mw>
2024-07-11 10:57:56 +0200

    Done with sing in page

--- src/Login&SignUpPages/Aside/Aside.jsx
+++ src/Login&SignUpPages/Aside/Aside.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook,FaGithub,FaLinkedin } from "react-icons/fa";
 import './Aside.css'
 
 function Aside() {
@@ -7,7 +9,22 @@ function Aside() {
        <div className='sswapdiv'>
           <div className='ssffdiv1'>
                 <div id='mland'>
-
+                    <h1 id='dama'>Sign In</h1>
+                    <div id='icons'>
+                    <FcGoogle size={25}/>
+                    <FaFacebook size={25} color='blue'/>
+                    </div>
+                    <div id='icons'> 
+                    <FaGithub size={25} color='blue'/>
+                    <FaLinkedin size={25} color='blue'/>
+                    </div>
+                    <div id='singbox'>
+                    <p>Or use your Email and Password</p>
+                    <input id='singput' placeholder='Email'></input>
+                    <input id='singput' placeholder='Password'></input>
+                    <p id='kantu'>Forget your password?</p>
+                    </div>
+                    <button id='straight'>SIGN IN</button>
                 </div>
             </div>
             <div className='ssffdiv2'>
